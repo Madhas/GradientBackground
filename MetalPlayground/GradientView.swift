4df96238d975a57797a9884c567121599b0baf0a
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-01-19 17:15:32 +0300

    Implement a turbulent displacement draft

--- MetalPlayground/GradientView.swift
+++ MetalPlayground/GradientView.swift
@@ -10,9 +10,9 @@ import Metal
 import MetalPerformanceShaders
 import simd
 
-private struct ControlColor {
-    let color: simd_float4
-    let point: simd_float2
+private struct VertexIn {
+    let position: SIMD4<Float>
+    let displacementColor: SIMD4<Float>
 }
 
 final class GradientView: UIView {
@@ -26,20 +26,38 @@ final class GradientView: UIView {
     }
     
     private var device: MTLDevice!
-    private var pipelineState: MTLRenderPipelineState!
+    private var renderPipelineState: MTLRenderPipelineState!
+    private var computePipelineState: MTLComputePipelineState!
     private var commandQueue: MTLCommandQueue!
     private var library: MTLLibrary!
-    
+    private var displacementMapTexture: MTLTexture!
+    private var drawableCopy: MTLTexture!
+
     private var vertexBuffer: MTLBuffer!
-    private let vertices: [Float] = [
-        -1,  1, 0, 1,
-         1, -1, 0, 1,
-         1,  1, 0, 1,
+    private let vertices: [VertexIn] = {
+        let color1 = SIMD4<Float>(171 / 255, 171 / 255, 0, 1)
+        let color2 = SIMD4<Float>(0, 128 / 255, 0, 1)
+        let color3 = SIMD4<Float>(128 / 255, 0, 0, 1)
+        let color4 = SIMD4<Float>(0, 128 / 255, 0, 1)
         
-        -1,  1, 0, 1,
-         1, -1, 0, 1,
-        -1, -1, 0, 1,
-    ]
+        return [
+            VertexIn(position: SIMD4(-1, 1, 0, 1), displacementColor: color1),
+            VertexIn(position: SIMD4(1, 1, 0, 1), displacementColor: color1),
+            VertexIn(position: SIMD4(-1, 0, 0, 1), displacementColor: color1),
+            
+            VertexIn(position: SIMD4(1, 1, 0, 1), displacementColor: color2),
+            VertexIn(position: SIMD4(-1, 0, 0, 1), displacementColor: color2),
+            VertexIn(position: SIMD4(1, 0, 0, 1), displacementColor: color2),
+            
+            VertexIn(position: SIMD4(-1, 0, 0, 1), displacementColor: color3),
+            VertexIn(position: SIMD4(1, 0, 0, 1), displacementColor: color3),
+            VertexIn(position: SIMD4(-1, -1, 0, 1), displacementColor: color3),
+            
+            VertexIn(position: SIMD4(1, 0, 0, 1), displacementColor: color4),
+            VertexIn(position: SIMD4(-1, -1, 0, 1), displacementColor: color4),
+            VertexIn(position: SIMD4(1, -1, 0, 1), displacementColor: color4)
+        ]
+    }()
     
     private var colorsBuffer: MTLBuffer!
     private let colors: [SIMD4<Float>] = [
@@ -80,6 +98,16 @@ final class GradientView: UIView {
                 SIMD2(point.x * Float(self.bounds.width), point.y * Float(self.bounds.height))
             }
             controlPointsBuffer = device.makeBuffer(bytes: controlPoints, length: MemoryLayout.size(ofValue: controlPoints[0]) * controlPoints.count, options: [])
+            
+            let textureDescriptor = MTLTextureDescriptor()
+            textureDescriptor.pixelFormat = .bgra8Unorm
+            textureDescriptor.width = Int(self.bounds.width)
+            textureDescriptor.height = Int(self.bounds.height)
+            textureDescriptor.usage = [.renderTarget, .shaderRead]
+            displacementMapTexture = device.makeTexture(descriptor: textureDescriptor)
+            
+            textureDescriptor.usage = .shaderRead
+            drawableCopy = device.makeTexture(descriptor: textureDescriptor)
         }
     }
     
@@ -98,12 +126,16 @@ final class GradientView: UIView {
         
         let vertexProgram = library.makeFunction(name: "vertex_shader")!
         let fragmentProgram = library.makeFunction(name: "fragment_shader")!
+        let computeProgram = library.makeFunction(name: "displaceTexture")!
         
-        let desc = MTLRenderPipelineDescriptor()
-        desc.vertexFunction = vertexProgram
-        desc.fragmentFunction = fragmentProgram
-        desc.colorAttachments[0].pixelFormat = .bgra8Unorm
-        pipelineState = try! device.makeRenderPipelineState(descriptor: desc)
+        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+        renderPipelineDescriptor.vertexFunction = vertexProgram
+        renderPipelineDescriptor.fragmentFunction = fragmentProgram
+        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        renderPipelineDescriptor.colorAttachments[1].pixelFormat = .bgra8Unorm
+        renderPipelineState = try! device.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        
+        computePipelineState = try! device.makeComputePipelineState(function: computeProgram)
         
         displayLink = CADisplayLink(target: self, selector: #selector(tick))
         displayLink.add(to: .main, forMode: .default)
@@ -120,24 +152,60 @@ final class GradientView: UIView {
         
         let commandBuffer = commandQueue.makeCommandBuffer()!
         
+        let clearWhite = MTLClearColor(red: 1, green: 1, blue: 1, alpha: 1)
         let renderPassDescriptor = MTLRenderPassDescriptor()
         renderPassDescriptor.colorAttachments[0].texture = drawable.texture
         renderPassDescriptor.colorAttachments[0].loadAction = .clear
         renderPassDescriptor.colorAttachments[0].storeAction = .store
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 1, green: 1, blue: 1, alpha: 1)
+        renderPassDescriptor.colorAttachments[0].clearColor = clearWhite
+        
+        if let texture = displacementMapTexture {
+            renderPassDescriptor.colorAttachments[1].texture = texture
+            renderPassDescriptor.colorAttachments[1].loadAction = .dontCare
+            renderPassDescriptor.colorAttachments[1].storeAction = .dontCare
+            renderPassDescriptor.colorAttachments[1].clearColor = clearWhite
+        }
         
         let encoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)!
-        encoder.setRenderPipelineState(pipelineState)
+        encoder.setRenderPipelineState(renderPipelineState)
         encoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         encoder.setFragmentBuffer(colorsBuffer, offset: 0, index: 0)
         encoder.setFragmentBuffer(controlPointsBuffer, offset: 0, index: 1)
-        encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 6)
+        encoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
         encoder.endEncoding()
         
+        let blur = MPSImageGaussianBlur(device: device, sigma: 30)
+        blur.edgeMode = .clamp
+        blur.encode(commandBuffer: commandBuffer, inPlaceTexture: &renderPassDescriptor.colorAttachments[0].texture!, fallbackCopyAllocator: nil)
+
         if usesBlur {
-            let blur = MPSImageGaussianBlur(device: device, sigma: 30)
-            blur.edgeMode = .clamp
-            blur.encode(commandBuffer: commandBuffer, inPlaceTexture: &renderPassDescriptor.colorAttachments[0].texture!, fallbackCopyAllocator: nil)
+//            let blitEncoder = commandBuffer.makeBlitCommandEncoder()!
+//            blitEncoder.copy(from: drawable.texture,
+//                             sourceSlice: 0,
+//                             sourceLevel: 0,
+//                             sourceOrigin: MTLOrigin(x: 0, y: 0, z: 0),
+//                             sourceSize: MTLSize(width: drawable.texture.width, height: drawable.texture.height, depth: 1),
+//                             to: drawableCopy,
+//                             destinationSlice: 0,
+//                             destinationLevel: 0,
+//                             destinationOrigin: MTLOrigin(x: 0, y: 0, z: 0))
+//            blitEncoder.endEncoding()
+            
+            let compute = commandBuffer.makeComputeCommandEncoder()!
+            compute.setComputePipelineState(computePipelineState)
+            compute.setTexture(drawable.texture, index: 0)
+            compute.setTexture(displacementMapTexture, index: 1)
+            compute.setTexture(drawable.texture, index: 2)
+            
+            let w = computePipelineState.threadExecutionWidth;
+            let h = computePipelineState.maxTotalThreadsPerThreadgroup / w;
+            let threadsPerThreadgroup = MTLSize(width: w, height: h, depth: 1);
+            let threadgroupsPerGrid = MTLSize(width: (drawable.texture.width + w - 1) / w,
+                                              height: (drawable.texture.height + h - 1) / h,
+                                              depth: 1)
+            
+            compute.dispatchThreadgroups(threadsPerThreadgroup, threadsPerThreadgroup: threadgroupsPerGrid)
+            compute.endEncoding()
         }
         
         commandBuffer.present(drawable)
--- MetalPlayground/Shaders.metal
+++ MetalPlayground/Shaders.metal
@@ -11,29 +11,41 @@ using namespace metal;
 
 struct VertexIn {
     float4 position;
+    float4 displacementColor;
 };
 
 struct VertexOut {
     float4 position [[ position ]];
+    float4 displacementColor;
+};
+
+struct FragmentOut {
+    float4 color [[ color(0) ]];
+    float4 displacementColor [[ color(1) ]];
 };
 
 vertex VertexOut vertex_shader(device const VertexIn *vertices [[ buffer(0) ]],
                                uint vid [[ vertex_id ]]) {
     VertexOut out;
     out.position = vertices[vid].position;
+    out.displacementColor = vertices[vid].displacementColor;
     return out;
 }
 
-fragment float4 fragment_shader(const VertexOut in [[ stage_in ]],
-                                constant const float4 *colors [[ buffer(0) ]],
-                                constant const float2 *controlPoints [[ buffer(1) ]]) {
+fragment FragmentOut fragment_shader(const VertexOut in [[ stage_in ]],
+                                     constant const float4 *colors [[ buffer(0) ]],
+                                     constant const float2 *controlPoints [[ buffer(1) ]]) {
+    FragmentOut out;
+    out.displacementColor = in.displacementColor;
+    
     float2 position = float2(in.position.x, in.position.y);
     float dists[4];
     float total = 0;
     for (int i = 0; i < 4; ++i) {
         float d = distance(controlPoints[i], position);
         if (d == 0) {
-            return colors[i];
+            out.color = colors[i];
+            return out;
         }
         
         d = 1 / (d * d);
@@ -47,5 +59,36 @@ fragment float4 fragment_shader(const VertexOut in [[ stage_in ]],
         color += ratio * colors[i];
     }
     
-    return color;
+    out.color = color;
+    return out;
+}
+
+kernel void displaceTexture(texture2d<float, access::read> source [[ texture(0) ]],
+                            texture2d<float, access::read> map [[ texture(1) ]],
+                            texture2d<float, access::write> dest [[ texture(2) ]],
+                            uint2 pos [[ thread_position_in_grid ]]) {
+    uint maxWidth = dest.get_width();
+    uint maxHeight = dest.get_height();
+//    if (pos.x >= maxWidth || pos.y >= maxHeight) {
+//        return;
+//    }
+    pos.x = pos.x >= maxWidth ? pos.x - maxWidth : pos.x;
+    pos.y = pos.y >= maxHeight ? pos.y - maxHeight : pos.y;
+    
+    float amount = 30;
+    
+    float4 sourceColor = source.read(pos);
+    float4 displacementColor = map.read(pos);
+    
+    float dx = amount * ((displacementColor[0] - 128) / 128);
+    float dy = amount * ((displacementColor[1] - 128) / 128);
+    
+    uint x = pos.x + dx;
+    uint y = pos.y + dy;
+    uint2 destPos = uint2(clamp(x, uint(0), maxWidth),
+                          clamp(y, uint(0), maxHeight));
+    
+    dest.write(sourceColor, destPos);
+    
+//    dest.write(float4(0, 1, 0, 1), destPos);
 }
