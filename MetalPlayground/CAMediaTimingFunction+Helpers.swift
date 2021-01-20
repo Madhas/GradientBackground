b0b045e6a5a6465a754bdd48f334b910fc843613
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-01-20 18:14:50 +0300

    Add CAMediaTimingFunction extension

---
+++ MetalPlayground/CAMediaTimingFunction+Helpers.swift
@@ -0,0 +1,39 @@
+//
+//  CAMediaTimingFunction+Helpers.swift
+//  MetalPlayground
+//
+//  Created by Андрей Овсянников on 19.01.2021.
+//
+
+import UIKit
+
+extension CAMediaTimingFunction {
+    
+    func slopeFor(t: Float) -> Float {
+        var p1: [Float] = [0, 0];
+        var p2: [Float] = [0, 0]
+        getControlPoint(at: 1, values: &p1)
+        getControlPoint(at: 2, values: &p2)
+        let ctrl1 = CGPoint(x: Double(p1[0]), y: Double(p1[1]))
+        let ctrl2 = CGPoint(x: Double(p2[0]), y: Double(p2[1]))
+        
+        let a1 = pow((1 - t), 3) * CGPoint.zero
+        let a2 = pow(3 * (1 - t), 2) * t * ctrl1
+        let a3 = 3 * (1 - t) * pow(t, 2) * ctrl2
+        let a4 = pow(t, 3) * CGPoint(x: 1, y: 1)
+        let point = a1 + a2 + a3 + a4
+        
+        return Float(point.y / point.x)
+    }
+}
+
+private extension CGPoint {
+
+    static func *(lhs: Float, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: Double(lhs) * Double(rhs.x), y: Double(lhs) * Double(rhs.y))
+    }
+
+    static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+}
