ecff652b7b038109fca6de4f9ffbbc3a2c6674d0
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-01-27 20:36:38 +0300

    Correct ColorSettings

--- MetalPlayground/ColorSettingsCell.swift
+++ MetalPlayground/ColorSettingsCell.swift
@@ -12,41 +12,52 @@ final class ColorSettingsCell: UICollectionViewCell {
     private let gradientView: GradientView
     private let titleContainerView: UIView
     private let titleLabel: UILabel
+    private let separatorView: UIView
     
     override init(frame: CGRect) {
         let config = GradientViewConfig(colors: Settings.shared.selectedColors)
         gradientView = GradientView(config: config)
         titleContainerView = UIView()
         titleLabel = UILabel()
+        separatorView = UIView()
         
         super.init(frame: frame)
         
         backgroundColor = .white
         
         addSubview(gradientView)
-        
-        titleContainerView.backgroundColor = .white
         addSubview(titleContainerView)
         
         titleLabel.text = "Edit colors"
         titleLabel.font = .systemFont(ofSize: 17)
-        titleLabel.textAlignment = .center
         titleContainerView.addSubview(titleLabel)
+        
+        separatorView.backgroundColor = .lightGray
+        addSubview(separatorView)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    override var isHighlighted: Bool {
+        didSet {
+            backgroundColor = isHighlighted ? UIColor.gray.withAlphaComponent(0.6) : .white
+        }
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        let bottomHeight = min(34, bounds.height)
+        let bottomHeight = min(48, bounds.height)
         titleContainerView.frame = CGRect(x: 0, y: bounds.maxY - bottomHeight, width: bounds.width, height: bottomHeight)
         
         let titleWidth = titleLabel.sizeThatFits(CGSize(width: .greatestFiniteMagnitude, height: bottomHeight)).width
-        titleLabel.frame = CGRect(x: titleContainerView.bounds.midX - titleWidth / 2, y: 0, width: titleWidth, height: bottomHeight)
+        titleLabel.frame = CGRect(x: 14, y: 0, width: titleWidth, height: bottomHeight)
         
         gradientView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height - bottomHeight)
+        
+        let pixel = 1 / UIScreen.main.scale
+        separatorView.frame = CGRect(x: 14, y: bounds.maxY - pixel, width: bounds.width - 14, height: pixel)
     }
 }
