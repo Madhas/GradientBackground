8a40af7995a6b83d622c46f6e102e76cff355f77
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-01-30 17:01:26 +0300

    Update EditColorsController

--- MetalPlayground/EditColorsController.swift
+++ MetalPlayground/EditColorsController.swift
@@ -9,5 +9,60 @@ import UIKit
 
 final class EditColorsController: UIViewController {
     
-
+    private let bottomHeight: CGFloat = 48
+    
+    var gradientView: GradientView!
+    
+    private var containerView: UIView!
+    private var applyButton: UIButton!
+    private var defaultsButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if gradientView == nil {
+            let config = GradientViewConfig(colors: Settings.shared.selectedColors)
+            gradientView = GradientView(config: config)
+            view.addSubview(gradientView)
+        }
+        
+        containerView = UIView()
+        containerView.backgroundColor = .white
+        view.addSubview(containerView)
+        
+        applyButton = UIButton(type: .system)
+        applyButton.setTitle("Apply", for: .normal)
+        applyButton.titleLabel?.font = .systemFont(ofSize: 17)
+        applyButton.setTitleColor(.mainColor, for: .normal)
+        containerView.addSubview(applyButton)
+        
+        defaultsButton = UIButton(type: .system)
+        defaultsButton.setTitle("Defaults", for: .normal)
+        defaultsButton.titleLabel?.font = .systemFont(ofSize: 17)
+        defaultsButton.setTitleColor(.mainColor, for: .normal)
+        containerView.addSubview(defaultsButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let bottomHeight: CGFloat
+        if #available(iOS 11, *) {
+            bottomHeight = self.bottomHeight + view.safeAreaInsets.bottom
+        } else {
+            bottomHeight = self.bottomHeight
+        }
+        
+        containerView.frame = CGRect(x: 0,
+                                     y: view.bounds.height - bottomHeight,
+                                     width: view.bounds.width,
+                                     height: bottomHeight)
+        gradientView.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - bottomHeight)
+        
+        applyButton.frame = CGRect(x: 0, y: 0, width: containerView.bounds.width / 2, height: self.bottomHeight)
+        defaultsButton.frame = CGRect(x: containerView.bounds.width / 2,
+                                      y: 0,
+                                      width: containerView.bounds.width / 2,
+                                      height: self.bottomHeight)
+    }
 }
