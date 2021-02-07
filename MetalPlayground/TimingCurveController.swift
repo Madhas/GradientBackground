dc7e315b83b9f2a9eb47c358693f9c45eee1677c
Andrey Ovsyannikov <bankid14@yandex.ru>
2021-02-07 16:21:37 +0300

    Add TimingCurveController

---
+++ MetalPlayground/TimingCurveController.swift
@@ -0,0 +1,83 @@
+//
+//  TimingCurveController.swift
+//  MetalPlayground
+//
+//  Created by Андрей Овсянников on 07.02.2021.
+//
+
+import UIKit
+
+final class TimingCurveController: UIViewController {
+    
+    private var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Timing Curve"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(close))
+        navigationItem.rightBarButtonItem?.tintColor = .mainColor
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 0
+        
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: layout)
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .white
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        view.addSubview(collectionView)
+        
+        collectionView.register(TimingCurveSelectionCell.self, forCellWithReuseIdentifier: String(describing: TimingCurveSelectionCell.self))
+    }
+
+    // MARK: Actions
+    
+    @objc private func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+// MARK: UICollectionViewDataSource
+
+extension TimingCurveController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        switch indexPath.item {
+        case 0:
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: TimingCurveSelectionCell.self), for: indexPath) as! TimingCurveSelectionCell
+            cell.delegate = self
+            return cell
+        default:
+            fatalError("Unexpected number of cells")
+        }
+    }
+}
+
+// MARK: UICollectionViewDelegateFlowLayout
+
+extension TimingCurveController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        switch indexPath.item {
+        case 0:
+            return CGSize(width: collectionView.bounds.width, height: 200)
+        default:
+            fatalError("Unexpected number of cells")
+        }
+    }
+}
+
+// MARK: TimingCurveSelectionCellDelegate
+
+extension TimingCurveController: TimingCurveSelectionCellDelegate {
+    
+    func timingCurveSelectionCell(_ cell: TimingCurveSelectionCell, didSelectTitle title: String, value: CAMediaTimingFunction?) {
+        
+    }
+}
